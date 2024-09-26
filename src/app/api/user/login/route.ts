5420503ac69ca74a965648b5abc4d268dee20f42
Mathilbzel <buachompoo_r@cmu.ac.th>
2024-09-26 14:41:59 +0700

    Hell pleaseeee

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -1,18 +1,16 @@
 import jwt from "jsonwebtoken";
 
-import { DB, readDB,Database } from "@lib/DB";
+import { Database, DB, readDB } from "@lib/DB";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
-  readDB();
   const body = await request.json();
   const { username, password } = body;
-
+  readDB();
   const user = (<Database>DB).users.find(
     (user) => user.username === username && user.password === password
   );
-
-  if (!user)
+  if (!user) {
     return NextResponse.json(
       {
         ok: false,
@@ -20,14 +18,15 @@ export const POST = async (request: NextRequest) => {
       },
       { status: 400 }
     );
+  }
+  const secret = process.env.JWT_SECRET || "This is my special secret";
 
-    const secret = process.env.JWT_SECRET || "This is another secret";
+  const token = jwt.sign(
+    { username, role: user.role},
+    secret,
+    { expiresIn: "8h" }
+  );
 
-    const token = jwt.sign(
-      { role: user.role },
-      secret,
-      { expiresIn: "8h" }
-    );
 
   return NextResponse.json({ ok: true, token });
 };
